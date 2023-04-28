7a6fb6c9882436f3ceaaa059ad931cbbfa5b9eeb
Saugat Nepal <sandnnepal4972@gmail.com>
2023-04-29 02:15:03 +0545

    Update automate.sh

--- automate.sh
+++ automate.sh
@@ -3,7 +3,7 @@ read domain
 echo "Finding subdomain using subfinder of $domain ##########"
 subfinder -d $domain -o subfinder.txt
 echo "Finding subdomain using amass of $domain ##########"
-amass enum -d $domain -o amass.txt
+amass enum -d $domain > amass.txt
 
 echo "Merging subdomains of subfinder and amass ##########"
 echo "Removing Duplicate subdomains ##########"
